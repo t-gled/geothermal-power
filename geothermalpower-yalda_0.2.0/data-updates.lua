8de8c0163558a06e92062fd62bdc758a84a923e9
Thomas Gledhill <thomasjgledhill@gmail.com>
2022-04-05 17:17:19 +0100

    added steam condensation recipe

--- geothermalpower-yalda_0.2.0/data-updates.lua
+++ geothermalpower-yalda_0.2.0/data-updates.lua
@@ -1,2 +1,3 @@
 -- modifying existing techs
 table.insert(data.raw.technology["fluid-handling"].effects, {type = "unlock-recipe", recipe = "geothermal-derrick"})
+table.insert(data.raw.technology["oil-processing"].effects, {type = "unlock-recipe", recipe = "steam-condensation"})
--- geothermalpower-yalda_0.2.0/prototypes/recipe.lua
+++ geothermalpower-yalda_0.2.0/prototypes/recipe.lua
@@ -10,5 +10,26 @@ data:extend({
     result = "geothermal-derrick",
     },
 
+    { --steam condensation
+        type = "recipe",
+        name = "steam-condensation",
+        category = "chemistry",
+        enabled = false,
+        energy_required = 2,
+        ingredients = {
+            {type = "fluid", name = "steam", amount = 20},
+        },
+        results = {
+            {type = "fluid", name = "water", amount = 20},
+        },
+        subgroup = "fluid-recipes",
+        crafting_machine_tint = {
+            primary = {r = 82, g = 180, b = 217},
+            secondary = {r = 98, g = 171, b = 199},
+            tertiary = {r = 201, g = 208, b = 209},
+            quaternary = {r = 210, g = 219, b = 220},
+        },
+    },
+
 }
 )
