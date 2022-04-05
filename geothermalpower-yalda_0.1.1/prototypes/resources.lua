8e41a3586cb2c923f98011572679a006df159d6b
Thomas Gledhill <thomasjgledhill@gmail.com>
2022-04-05 15:28:56 +0100

    geothermal vent animations and better textures

--- geothermalpower-yalda_0.1.1/prototypes/resources.lua
+++ geothermalpower-yalda_0.1.1/prototypes/resources.lua
@@ -43,18 +43,35 @@ data:extend({
             }
             }
         },
-        stage_counts = {0},
-        stages  ={
+        stage_counts = {6330, 1930, 270, 50},
+        stages  = {
             sheet = {
                 filename = "__geothermalpower-yalda__/graphics/resource/geothermal-vent/geothermal-vent.png",
                 priority = "extra-high",
                 width = 74,
                 height = 60,
                 frame_count = 4,
-                variation_count = 1,
+                variation_count = 4,
                 shift = util.by_pixel(0, -2),
             },
         },
+        stages_effect = {
+            sheet = {
+                filename = "__geothermalpower-yalda__/graphics/entity/vent-steam/vent-steam.png",
+                priority = "extra-high",
+                width = 152,
+                height = 120,
+                frame_count = 4,
+                variation_count = 4,
+                blend_mode = "additive",
+                flags = {"smoke"},
+            },
+        },
+        effect_animation_period = 5,
+        effect_animation_period_deviation = 1,
+        effect_darkness_multiplier = 1,
+        min_effect_alpha = 0.4,
+        max_effect_alpha = 0.7,
         map_color = {r = 210, g = 219, b = 220},
         map_grid = false
     },
