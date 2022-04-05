2d41816ac82b2857d1c528f1ab553b6b5d5655ea
Thomas Gledhill <thomasjgledhill@gmail.com>
2022-04-05 21:32:48 +0100

    fixed error

--- data-final-fixes.lua
+++
@@ -1,4 +0,0 @@
---steinios unlasting oil compatibility per request
-if mods["steinios_unlasting_oil"] then
-    table.insert(suo_resources, {dependency = "geothermalpower-yalda", name = "geothermal-vent", default = true, fluid = true})
-end
